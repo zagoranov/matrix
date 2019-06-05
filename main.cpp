0313f73d6480ba47f1ba2ff06bb30e5cf1f2bdf6
zagoranov <pijammer@gmail.com>
2019-06-05 23:04:59 +0300

    Tatyana's corrections

--- main.cpp
+++ main.cpp
@@ -7,27 +7,24 @@
 template<typename T, T defValue>
 class Matrix {
 	using RetTuple = std::tuple<long, long, T>;
-	friend class ref;
-	
 	std::map<std::pair<long, long>, T> MatrixData;
 	T dvalue;
 
 public:
 	Matrix() { 
-		MatrixData.clear(); 
 		dvalue = defValue;
 	}
 	~Matrix() {}
-	size_t size() { return MatrixData.size(); }
+	size_t size() const { return MatrixData.size(); }
 
-	//
+	//[][]
 	class ref
 	{
 		Matrix& cont_;
 		long row_;
 		long column_;
 	public:
-		ref(Matrix& cont, long i) : cont_(cont), row_(i) {}
+		ref(Matrix& cont, long i) : cont_(cont), row_(i),column_(-1) {}
 		operator const T& () const { 
 			auto pos = std::make_pair(row_, column_);
 			auto it = cont_.MatrixData.find(pos);
@@ -38,19 +35,23 @@ public:
 		}
 		ref& operator [](long i)
 		{
+			if (column_ > -1)
+				throw std::overflow_error("Too much index arguments");
 			column_ = i;
 			return *this;
 		}
 		ref& operator=(T value)
 		{
+			if(column_ < 0)
+				throw std::overflow_error("Not enough index arguments");
 			auto pos = std::make_pair(row_, column_);
 			auto it = cont_.MatrixData.find(pos);
 			if (value == cont_.dvalue) {
-				if(it != cont_.MatrixData.end())
+				if (it != cont_.MatrixData.end())
 					cont_.MatrixData.erase(it);
 			}
 			else
-				cont_.MatrixData[pos] = value;
+				cont_.MatrixData.insert({ pos, value });
 			return *this;
 		}
 	};
@@ -59,17 +60,16 @@ public:
 		return ref(*this, i);
 	}
 
-	// 
 	class iterator {
 	public:
 		typename std::map<std::pair<long, long>, T>::iterator it;
 		iterator(typename std::map<std::pair<long, long>, T>::iterator _it) : it(_it) {}
 		bool operator!=(const iterator & other) const {	return it != other.it;	}
-		iterator operator++() {	it++; 	return *this; }
+		iterator operator++() {	++it; 	return *this; }
 		RetTuple operator*() { 
 			long l1 = (*it).first.first;
 			long l2 = (*it).first.second;
-			return std::make_tuple<long, long, T>(std::move(l1), std::move(l2), std::move((*it).second));
+			return std::make_tuple(l1, l2, (*it).second);
 		}
 	};
 	iterator begin() { return MatrixData.begin(); }
@@ -77,18 +77,19 @@ public:
 };
 
 
+
 int main()
 {
 	Matrix<int, 0> matrix;
-	assert(matrix.size() == 0); 
+	assert(matrix.size() == 0); 
 
 	for (size_t i = 0; i < 10; ++i)
 		matrix[i][i] = i;
 	for(size_t i = 0, j = 9; i < 10; ++i, j--)
 		matrix[i][j] = i;
 
-	for (size_t i = 0; i < 10; ++i) {
-		for (size_t j = 0; j < 10; ++j)
+	for (size_t i = 1; i < 9; ++i) {
+		for (size_t j = 1; j < 9; ++j)
 			std::cout << matrix[i][j] << " ";
 		std::cout << std::endl;
 	}
@@ -102,6 +103,7 @@ int main()
 		int v;
 		std::tie(x, y, v) = c;
 		std::cout << "[" << x <<"]["<< y << "] = " << v << "; ";
-	}
-	//std::system("pause");
+	}
+
+	//int i;  std::cin >> i;
 }
